eedfc26d8f1d064088ba7eff6a1d97de8f40d4d8
Adam Wójs <adam@wojs.pl>
2019-08-15 14:00:42 +0200

    Initial commit

---
+++ src/bundle/EzPlatformOAuth2Bundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdamWojs\EzPlatformOAuth2Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class EzPlatformOAuth2Bundle extends Bundle
+{
+}
